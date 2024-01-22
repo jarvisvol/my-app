3b7d8bf94ed0dec33310663db83624534bef0c9f
Shubham Singh <s.singh@agilecockpit.com>
2024-01-22 20:50:33 +0530

    lklk

---
+++ src/components/profile/Profile.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './index.css'
+// import profileimg from '../../images/profileimg.jpg';
+import { Divider } from 'semantic-ui-react';
+
+export default function Profile() {
+    return (
+        <div className='profile-container'>
+            <div className='uper-section'>
+                <div className='profile-circle'>
+                    <span className='position-text'>Full Stack Developer at Xcelorn.nl</span>
+                </div>
+            </div>
+            <Divider />
+        </div>
+    )
+}
