56b330b6983b73540eb1357b4462a9ad69c1c456
Shubham Singh <s.singh@agilecockpit.com>
2024-02-01 10:37:01 +0530

    ch

--- src/components/Navbar/pages/Navbar.js
+++ src/components/Navbar/pages/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
     <div>
       <div className='left-nav-container'>
         <div className='portfolio-conatiner'>
-          <p className='size-16' style={{ padding: '5px 5px 4px 14px' }}> yyyyy</p>
+          <p className='size-16' style={{ padding: '5px 5px 4px 14px' }}> Portfolio</p>
         </div>
         <div className='project-list-container size-16'>
           <span className='size-16' style={{ padding: '5px 5px 4px 14px' }}>Project List</span>
