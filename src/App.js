0f8ae45706dc2b2e800c82f230787d8a67735258
Shubham Singh <s.singh@agilecockpit.com>
2024-02-04 23:32:46 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react'
 // import Navbar from './components/Navbar/pages/Navbar'
 import './common_css/index.css'
 import Profile from './components/profile/Profile'
+import Chart from './components/Project/Chart'
 
 
 export default function App() {
   return (
     <div className='app-container' style={{display:'flex'}}>
-    {/* <Navbar/> */}
     <Profile/>
+    {/* <Chart/> */}
     </div>
   )
 }
