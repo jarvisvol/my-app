630fe35d5183d0b9d52b0cd56d74065ec78545ac
Shubham Singh <s.singh@agilecockpit.com>
2024-02-15 09:56:06 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './common_css/index.css'
 import Profile from './components/profile/Profile'
-import Chart from './components/Project/Chart'
+import BarChart from './components/Project/BarChart'
 
 
 export default function App() {
@@ -10,7 +10,7 @@ export default function App() {
     <BrowserRouter>
        <Routes>
         <Route path="/"  element={ <Profile/>}/>
-        <Route path="/match-data"  element={ <Chart/>}/>
+        <Route path="/match-data"  element={ <BarChart/>}/>
        </Routes>
     </BrowserRouter>
   )
