0d8c12583154ab39090c6a955de749cda36c4512
jarvisvol <shubham1410.ss@gmail.com>
2024-02-24 15:50:01 +0530

    responsive

--- src/components/profile/Resume.js
+++ src/components/profile/Resume.js
@@ -21,30 +21,30 @@ export default function Resume() {
                 <span style={{ width: '110px', height: '1px', borderTop: '1px solid black' }}>
 
                 </span>
-                <pre style={{ fontSize: '16px', fontWeight: 'bold', color: '#fffff', marginLeft: '154px' }}><h2> My Journey</h2></pre>
-                <div style={{ width: '390px', height: '1px', borderTop: '1px solid black', position: 'relative', left: '43vw' }}>
+                <span className='my-jounery-tag' style={{ fontSize: '16px', fontWeight: 'bold', color: '#fffff', marginLeft: '154px' }}><h2> My Journey</h2></span>
+                <div className='boder-of-jurney-tag' style={{ width: '390px', height: '1px', borderTop: '1px solid black', position: 'relative', left: '36vw' }}>
 
                 </div>
             </div>
             <br></br>
             <div>
-                <pre style={{ margin: 'auto', width: 'fit-content', position: 'relative', left: '44px' }}>
-                    <p>
-                        Hey, I'm Shubham Singh Dynamic Full Stack Developer with a passion<br /> for crafting innovative and efficient solutions in web development.
-                        With a solid foundation in both <br /> front-end and back-end technologies, seamlessly navigates the complexities of modern software development <br /> to create robust and user-friendly applications.
-                        Armed with extensive knowledge of <br /> programming languages such as JavaScript, Python, and NodeJS.
+                <span style={{ margin: 'auto' }}>
+                    <p style={{paddingLeft: '10px', paddingRight: '14px'}}>
+                        Hey, I'm Shubham Singh Dynamic Full Stack Developer with a passion for crafting innovative and efficient solutions in web development.
+                        With a solid foundation in both  front-end and back-end technologies, seamlessly navigates the complexities of modern software development  to create robust and user-friendly applications.
+                        Armed with extensive knowledge of  programming languages such as JavaScript, Python, and NodeJS.
                     </p>
-                </pre>
+                </span>
             </div>
 
-            <div style={{ display: 'flex' }}>
+            <div className='whole-resum-container' style={{ display: 'flex' }}>
                 <div className='experince-tag'>
                     <h4>Experince</h4>
                     <b>Agile Cockpit Software Pvt Ltd- Technical Developer</b>
-                    <pre>Gurugram India</pre>
+                    <span>Gurugram India</span>
                     <i>12/22- current</i>
-                    <div style={{ marginTop: '103px' }}>
-                        <pre><h4><b>Live Projects</b></h4></pre>
+                    <div className='project-list-container' style={{ marginTop: '103px' }}>
+                        <span><h4><b>Live Projects</b></h4></span>
                         {
                             projectArr?.map((item, i) => (
                                 <div key={i} style={{ cursor: 'pointer' }}> {
@@ -56,8 +56,8 @@ export default function Resume() {
                     </div>
 
                 </div>
-                <div style={{ position: 'relative', left: '64vw', height: '130px', borderLeft: '1px solid black', marginTop: '39px', paddingLeft: '60px' }}>
-                    <pre><h4><b>Skills</b></h4></pre>
+                <div className='skills-wala-container' style={{ position: 'relative', left: '66vw', height: '130px', marginTop: '39px', paddingLeft: '60px' }}>
+                    <span><h4><b>Skills</b></h4></span>
                     <div>
                         {
                             skills?.map((item,i) => (
@@ -66,12 +66,12 @@ export default function Resume() {
                         }
                     </div>
                 </div>
-                <div style={{ position: 'relative', left: '-213px' }}>
-                    <pre style={{ marginLeft: ' 20px' }}><h6 >Summary</h6></pre>
+                <div className='summuray-container' style={{ position: 'relative', left: '-213px' }}>
+                    <span style={{ marginLeft: ' 20px' }}><h6 >Summary</h6></span>
                     {
 
                         mybullet?.map((item, i) => (
-                            <pre key={i}> {item}</pre>
+                            <span key={i} style={{display:'inline-block'}}> {item}</span>
                         ))
                     }
                 </div>
